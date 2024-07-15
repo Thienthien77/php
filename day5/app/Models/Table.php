437886745222548ef3ac155930a5fc8c01f6a156
dvTinh99 <dvtinh.it@gmail.com>
2024-07-15 15:15:08 +0700

    migrate seed and factory

--- day5/app/Models/Table.php
+++ day5/app/Models/Table.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,21 @@ class Table extends Model
 
     public $timestamps = false;
     use HasFactory;
+
+    // protected function customerName(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn (mixed $value) => "tinhdoan",
+    //         set: fn (string $value) => "ô nô",
+    //     );
+    // }
+
+    // protected function getCustomerNameAttribute($value) {
+    //     return "tinhdaon";
+    // }
+
+    // protected function setCustomerNameAttribute($value) {
+    //     $this->attributes['customer_name'] = "tinhdoan da o day";
+    // }
+
 }
